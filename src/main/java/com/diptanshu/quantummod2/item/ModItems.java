097c7a69756e22a0c1f3ce6c22446f86a8311508
Diptanshu <dssikdar@gmail.com>
2022-07-06 02:08:20 -0700

    Created ResetQubitItem for convenience

--- src/main/java/com/diptanshu/quantummod2/item/ModItems.java
+++ src/main/java/com/diptanshu/quantummod2/item/ModItems.java
@@ -1,10 +1,7 @@
 package com.diptanshu.quantummod2.item;
 
 import com.diptanshu.quantummod2.QuantumMod2;
-import com.diptanshu.quantummod2.item.custom.HadamardGateItem;
-import com.diptanshu.quantummod2.item.custom.MeasurementItem;
-import com.diptanshu.quantummod2.item.custom.NotGateItem;
-import com.diptanshu.quantummod2.item.custom.PhaseFlipGateItem;
+import com.diptanshu.quantummod2.item.custom.*;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -29,6 +26,9 @@ public class ModItems {
 
     public static final RegistryObject<PhaseFlipGateItem> PHASEFLIPGATEITEM = ITEMS.register("phaseflipgateitem",
             () -> new PhaseFlipGateItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<ResetQubitItem> RESETQUBITITEM = ITEMS.register("resetqubititem",
+            () -> new ResetQubitItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     // create a register
     public static void register(IEventBus eventBus) {
---
+++ src/main/java/com/diptanshu/quantummod2/item/custom/ResetQubitItem.java
@@ -0,0 +1,37 @@
+package com.diptanshu.quantummod2.item.custom;
+
+import com.diptanshu.quantummod2.block.custom.QubitBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+
+import static com.diptanshu.quantummod2.block.custom.QubitBlock.matrixMult;
+
+public class ResetQubitItem extends Item
+{
+    public ResetQubitItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        BlockPos positionClicked = pContext.getClickedPos();
+        Player player = pContext.getPlayer();
+        Block clickedBlock = pContext.getLevel().getBlockState(positionClicked).getBlock();
+
+        if (clickedBlock instanceof QubitBlock) {
+            QubitBlock qubitBlock = QubitBlock.class.cast(clickedBlock);
+            double[] currentState = qubitBlock.stateVector.clone();
+            player.sendMessage(new TextComponent(("Current State: " + currentState[0]) + " |0>   AND   " + (currentState[1]) + " |1>"), player.getUUID());
+            qubitBlock.stateVector[0] = 1.0;
+            qubitBlock.stateVector[1] = 0.0;
+            player.sendMessage(new TextComponent(("New State: " + qubitBlock.stateVector[0]) + " |0>   AND   " + (qubitBlock.stateVector[1]) + " |1>"), player.getUUID());
+        }
+
+        return super.useOn(pContext);
+    }
+}
