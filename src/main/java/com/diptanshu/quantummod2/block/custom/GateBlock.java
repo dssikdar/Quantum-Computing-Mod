62c9549dbce4a92076ded49caee3696ff867722d
Foster Smith <fostersmith406@gmail.com>
2022-07-20 07:40:14 -0600

    added comments

--- src/main/java/com/diptanshu/quantummod2/block/custom/GateBlock.java
+++ src/main/java/com/diptanshu/quantummod2/block/custom/GateBlock.java
@@ -18,7 +18,6 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 
-import java.util.Properties;
 import java.util.Random;
 
 public abstract class GateBlock extends Block {
@@ -27,13 +26,13 @@ public abstract class GateBlock extends Block {
 
     public GateBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        // Set the default state to off
         this.registerDefaultState(this.stateDefinition.any()
-                .setValue(POWERED, Boolean.valueOf(false)));
-        System.out.println("Did the stuff");
+                .setValue(POWERED, false));
     }
 
     /**
-     * Return the result of the gate being applied to a qubit
+     * Return the result of the gate being applied to a qubit. Each child class must implement this.
      * @param statevector - the qubit statevector to be modified
      */
     protected abstract double[] apply(double[] statevector);
@@ -42,11 +41,14 @@ public abstract class GateBlock extends Block {
         return p_57062_ ? SoundEvents.STONE_BUTTON_CLICK_ON : SoundEvents.STONE_BUTTON_CLICK_OFF;
     }
 
+    // This is called on a right-click
     public InteractionResult use(BlockState state, Level level, BlockPos position, Player player, InteractionHand interactionHand, BlockHitResult hitResult) {
         if(!state.getValue(BlockStateProperties.POWERED)) {
+            // Set state to on and schedule tick to be powered off
             level.setBlock(position, state.setValue(BlockStateProperties.POWERED, true), 3);
             level.scheduleTick(position, this, pressDuration);
 
+            // Update all neighbouring blocks
             Block blockBelow = level.getBlockState(position.below(1)).getBlock();
             checkAndUpdate(level, blockBelow, "up");
 
@@ -63,24 +65,30 @@ public abstract class GateBlock extends Block {
             checkAndUpdate(level, blockFacingWest, "east");
 
             level.gameEvent(player, GameEvent.BLOCK_PRESS, position);
+            // Button on sound
             level.playSound(player, position, getSound(true), SoundSource.BLOCKS, 0.3F, 0.6F);
 
             return InteractionResult.SUCCESS;
         } else{
+            // Do nothing if already activated - this prevents super fast clicking
             return InteractionResult.CONSUME;
         }
 
     }
 
+    // This is called when the de-activate scheduled tick happens
     @Override
     public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Random random){
         if(blockState.getValue(BlockStateProperties.POWERED)){
             serverLevel.setBlock(blockPos, blockState.setValue(BlockStateProperties.POWERED, false), 3);
+            // Button off sound
             serverLevel.playSound(null, blockPos, getSound(false), SoundSource.BLOCKS, 0.3F, 0.6F);
             serverLevel.gameEvent(GameEvent.BLOCK_UNPRESS, blockPos);
 
         }
     }
+
+    // Apply the gate to a neighbouring qubit
     public void checkAndUpdate(Level level, Block directionalBlock, String face) {
         if (level.isClientSide()) {
             if (directionalBlock instanceof QubitBlock) {
@@ -94,6 +102,7 @@ public abstract class GateBlock extends Block {
         }
     }
 
+    // This is necessary for the block to hold the POWERED state
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
         builder.add(POWERED);
     }
